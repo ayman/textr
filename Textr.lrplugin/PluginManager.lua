1ce3a7159ac32eeede4d376bded499184b2b3f97
David A. Shamma <aymanshamma@gmail.com>
2017-03-20 19:59:16 +0100

    Cleaned up UI again.

--- Textr.lrplugin/PluginManager.lua
+++ Textr.lrplugin/PluginManager.lua
@@ -45,96 +45,113 @@ function PluginManager.sectionsForTopOfDialog(viewFactory, properties)
       {
          title = LOC "$$$/shamurai/textr/pluginName=Textr OCR Tagger",
          bind_to_object = prefs,
-         f:row {
-            spacing = f:control_spacing(),
-            f:static_text {
-               title = 'API Key (CLI):',
-               alignment = 'left',
-            },
-            f:edit_field {
-               immediate = true,
-               value_to_string = true,
-               alignment = 'left',
-               fill_horizontal = 1,
-               placeholder_value = LOC "$$$/shamurai/textr/gconf=<Please Configure In Google Clound Dashboard>",
-               tooltip = LOC "$$$/shamurai/textr/apifield=Should be a long API key of random letters and numbers.", 
-               value = LrView.bind('google_api_key'),
-            },
-            f:push_button {
-               width = 150,
-               title = LOC '$$$/shamurai/textr/gdash=Google Cloud Dashboard',
-               enabled = true,
-               action = function()
-                  LrHttp.openUrlInBrowser(_G.URL)
-               end,
+         f:group_box {
+            title = "About",
+            f:row{
+               f:static_text {
+                  title = "Recognize text in photos using the Google Cloud Vision API and add a searchable field for the photos. This came about as I was listening to the PetaPixel Podcast #57 and someone had a question about an automated way to tag race bib numbers in Lightroom. I thought it shouldn't be that hard to whip up in today's age...and here we are.",
+                  alignment = 'left',
+                  width_in_chars = 58,
+                  height_in_lines = -1,
+                  fill_horizontal = 1,
+               },
             },
          },
-         f:row {
-            spacing = f:control_spacing(),
-            f:static_text {
-               title = LOC '$$$/shamurai/textr/imgsize=Thumb size:',
-               alignment = 'left',
+         f:group_box {
+            title = "Configuration",
+            f:row {
+               spacing = f:control_spacing(),
+               f:static_text {
+                  title = 'API Key (CLI):',
+                  alignment = 'left',
+               },
+               f:edit_field {
+                  immediate = true,
+                  value_to_string = true,
+                  alignment = 'left',
+                  fill_horizontal = 1,
+                  width_in_digits = 40,
+                  placeholder_value = LOC "$$$/shamurai/textr/gconf=<Please Configure In Google Clound Dashboard>",
+                  tooltip = LOC "$$$/shamurai/textr/apifield=Should be a long API key of random letters and numbers.", 
+                  value = LrView.bind('google_api_key'),
+               },
+               f:push_button {
+                  width = 150,
+                  title = LOC '$$$/shamurai/textr/gdash=Google Cloud Dashboard',
+                  enabled = true,
+                  action = function()
+                     LrHttp.openUrlInBrowser(_G.URL)
+                  end,
+               },
             },
-            f:edit_field {
-               immediate = true,
-               string_to_value = getInt,
-               increment = 120,
-               precision = 0,
-               min = 120,
-               max = 2048,
-               alignment = 'right',
-               width_in_digits = 4,
-               value = LrView.bind('img_size'),
+            f:row {
+               spacing = f:control_spacing(),
+               f:static_text {
+                  title = LOC '$$$/shamurai/textr/imgsize=Thumb size:',
+                  alignment = 'left',
+               },
+               f:edit_field {
+                  immediate = true,
+                  string_to_value = getInt,
+                  increment = 120,
+                  precision = 0,
+                  min = 120,
+                  max = 2048,
+                  alignment = 'right',
+                  width_in_digits = 4,
+                  value = LrView.bind('img_size'),
+               },
+               f:static_text {
+                  title = LOC '$$$/shamurai/textr/maximgs=Batch size:',
+                  alignment = 'left',
+               },
+               f:edit_field {
+                  immediate = true,
+                  increment = 1,
+                  large_increment = 10,
+                  string_to_value = getInt,
+                  validate = getValidInt,
+                  precision = 0,
+                  min = 1,
+                  max = 1000,
+                  alignment = 'right',               
+                  width_in_digits = 3,               
+                  value = LrView.bind('max_imgs'),
+               },            
+               f:static_text {
+                  title = LOC '$$$/shamurai/textr/aregex=Allow Regex:',
+                  alignment = 'left',
+               },
+               f:combo_box {
+                  items = { "^[a-zA-Z0-9]+$", "^[0-9]+$", "^[a-zA-Z]+$" },
+                  tooltip = "Numbers & Letters: ^[a-zA-Z0-9]+$\nNumbers: ^[0-9]+$\nLetters: ^[a-zA-Z]+$",
+                  immediate = true,
+                  width_in_digits = 12,
+                  alignment = 'right',
+                  fill_horizontal = 1,
+                  value = LrView.bind('allow_regex'),
+               },            
+               f:static_text {
+                  title = LOC '$$$/shamurai/textr/tlength=Matched length:',
+                  alignment = 'left',
+               },
+               f:edit_field {
+                  increment = 1,
+                  large_increment = 2,
+                  immediate = true,
+                  string_to_value = getInt,
+                  precision = 0,
+                  min = 0,
+                  max = 10,
+                  alignment = 'right',
+                  width_in_digits = 3,               
+                  value = LrView.bind('text_length'),
+               },                        
             },
-            f:static_text {
-               title = LOC '$$$/shamurai/textr/maximgs=Batch size:',
-               alignment = 'left',
-            },
-            f:edit_field {
-               immediate = true,
-               increment = 1,
-               large_increment = 10,
-               string_to_value = getInt,
-               validate = getValidInt,
-               precision = 0,
-               min = 1,
-               max = 1000,
-               alignment = 'right',               
-               width_in_digits = 4,               
-               value = LrView.bind('max_imgs'),
-            },            
-            f:static_text {
-               title = LOC '$$$/shamurai/textr/aregex=Allow Regex:',
-               alignment = 'left',
-            },
-            f:combo_box {
-               items = { "^[a-zA-Z0-9]+$", "^[0-9]+$", "^[a-zA-Z]+$" },
-               tooltip = "Numbers & Letters: ^[a-zA-Z0-9]+$\nNumbers: ^[0-9]+$\nLetters: ^[a-zA-Z]+$",
-               immediate = true,
-               width_in_digits = 12,
-               alignment = 'right',
-               value = LrView.bind('allow_regex'),
-            },            
-            f:static_text {
-               title = LOC '$$$/shamurai/textr/tlength=Matched length:',
-               alignment = 'left',
-            },
-            f:edit_field {
-               increment = 1,
-               large_increment = 2,
-               immediate = true,
-               string_to_value = getInt,
-               precision = 0,
-               min = 0,
-               max = 10,
-               alignment = 'right',
-               width_in_digits = 8,               
-               value = LrView.bind('text_length'),
-            },                        
-         },
-         f:separator {
-            fill_horizontal = 1
-         },
+         },         
+         -- f:separator {
+         --    fill_horizontal = 1
+         -- },        
          f:row {
             f:static_text {
                title = 'By: ',
